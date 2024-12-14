a26e965172c272b83c0d06bd88acdd0ec8fa3627
developertarun01 <tarunusinessmail@gmail.com>
2024-12-14 12:21:22 +0530

    CSS

--- jan-seva-kendra-frontend/src/App.js
+++ jan-seva-kendra-frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
   };
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div className="bg-gray-100 flex flex-col min-h-screen">
         <Navbar />
 
         <main className="flex-grow">
